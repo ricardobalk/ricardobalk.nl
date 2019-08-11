6b850c73b6fa88959a241829b25e3eaf1044e6a5
ellipticcurv3 <14904229+ellipticcurv3@users.noreply.github.com>
2019-08-11 14:39:38 +0200

    New about section with Polaroid in Vue

--- src/_webpack/vue/app.js
+++ src/_webpack/vue/app.js
@@ -1,38 +1,40 @@
-
 // import moment from "moment"
 import 'lodash';
 import jQuery from 'jquery';
+
 window.$ = window.JQuery = jQuery;
 import './app.scss';
 import Vue from 'vue';
 import Hello from './vue-components/Hello.vue';
 import MyFooter from './vue-components/Footer.vue';
 import Greet from './vue-components/Greet.vue';
+import Polaroid from './vue-components/Polaroid.vue';
 
 const APPS = {
-  Hello,
-  MyFooter,
-  Greet
+    Hello,
+    MyFooter,
+    Greet,
+    Polaroid
 };
 
 // Vue.config.devtools = true
 
 function renderAppInElement(el) {
-  let App = APPS[el.id];
-  if (!App) return;
+    let App = APPS[el.id];
+    if (!App) return;
 
-  // Props as data attributes:
-  // <div class="__vue-root" data-message="Hello" id="Greet"></div>
-  const props = Object.assign({}, el.dataset);
-  console.log(props);
-  new Vue ({
-    el,
-    render(createElem) {
-      return createElem(App, {
-        attrs: props
-      });
-  }
-  })
+    // Props as data attributes:
+    // <div class="__vue-root" data-message="Hello" id="Greet"></div>
+    const props = Object.assign({}, el.dataset);
+    console.log(props);
+    new Vue({
+        el,
+        render(createElem) {
+            return createElem(App, {
+                attrs: props
+            });
+        }
+    })
 }
 
 document.querySelectorAll('.__vue-root').forEach(renderAppInElement);
@@ -52,14 +54,14 @@ function autorun() {
 }
 
 function darkmodeToggleEventHandler() {
- if (this.getAttribute("data-state") === "on") {
-     setDarkmode("on");
-     this.setAttribute("data-state", "off");
-  } else {
-     setDarkmode("off");
-     this.setAttribute("data-state", "on");
- }
- this.addEventListener("click", darkmodeToggleEventHandler, false);
+    if (this.getAttribute("data-state") === "on") {
+        setDarkmode("on");
+        this.setAttribute("data-state", "off");
+    } else {
+        setDarkmode("off");
+        this.setAttribute("data-state", "on");
+    }
+    this.addEventListener("click", darkmodeToggleEventHandler, false);
 }
 
 function setDarkmode(state) {
