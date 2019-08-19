fa3ead837d5d0e4f6ca1d5b26092e20fe9406c87
ellipticcurv3 <14904229+ellipticcurv3@users.noreply.github.com>
2019-08-20 01:54:50 +0200

    Fix window.location.hostname, we don't want to end in an infinite loop

--- src/_webpack/tbb-check/app.js
+++ src/_webpack/tbb-check/app.js
@@ -18,7 +18,7 @@ function isTorBrowser() {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    if (isTorBrowser()) {
+    if (window.location.hostname !== 'tj5amvcbpzcodr63.onion' && isTorBrowser()) {
         console.info("Detected Tor Browser Bundle! Redirecting to Tor site.");
         window.location = 'http://tj5amvcbpzcodr63.onion/';
     } else {
