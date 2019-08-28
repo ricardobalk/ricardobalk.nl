835f4f9bde9b5c0f12e987112e39834be586ed15
ellipticcurv3 <14904229+ellipticcurv3@users.noreply.github.com>
2019-08-28 23:10:15 +0200

    Fix rebuild script
    Forgot to add 'git fetch'

--- need-rebuild.sh
+++ need-rebuild.sh
@@ -3,6 +3,8 @@
 # This script checks whether the local directory is up-to-date with
 # the remote Git repository, and issues an `npm build` if necessary.
 
+git fetch;
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -13,7 +15,7 @@ if [ $LOCAL = $REMOTE ]; then
     exit 0;
 elif [ $LOCAL = $BASE ]; then
     printf "Need to pull - There are new changes\n";
-    git pull;
+    git pull --rebase;
     printf "Building site...\n";
     npm run build
     exit 0;
