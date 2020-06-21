98f9254e075a0192ae00d3d87a068f5ebbb7b505
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2020-06-21 16:05:38 +0200

    Added resolveSidebarItems to Layout.vue
    it fixes the build on 181acaf212bd90a9965f5289e75349171e8a23b9.

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -11,8 +11,14 @@
 <script>
   import Home from "@theme/components/Home.vue";
   import Page from "@theme/components/Page.vue";
+  import { resolveSidebarItems } from "../util";
 
   export default {
     components: { Home, Page },
+    computed: {
+      sidebarItems() {
+        return resolveSidebarItems(this.$page, this.$page.regularPath, this.$site, this.$localePath);
+      },
+    },
   };
 </script>
