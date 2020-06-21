38bd9129654f765e0cbd7ebfac238acd39cdfeae
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2020-06-21 12:53:14 +0200

    Converted BlogOverview to TypeScript
    ... and made it use the new language helper / retrievePosts mixins

--- src/.vuepress/theme/layouts/BlogOverview.vue
+++ src/.vuepress/theme/layouts/BlogOverview.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="blog-overview-container">
-    <BigHero
-      :title="pageTitle"
-      :image="heroImages.regular"
-      :image-dark="heroImages.dark"
-      class="blog-hero"
-    />
-
+    <BigHero :title="pageTitle" :image="heroImages.regular" :image-dark="heroImages.dark" class="blog-hero" />
     <section class="blog-overview">
       <div class="grid-x limited-width">
         <div class="small-offset-1 small-10 large-offset-2 large-8 cell">
@@ -17,46 +11,46 @@
   </div>
 </template>
 
-<script>
-  import BigHero from "@theme/components/BigHero";
-  import Articles from "@theme/components/Blog/Overview/Articles";
-  import retrievePosts from "@theme/mixins/retrievePosts";
+<script lang="ts">
+  import retrievePosts from "../mixins/retrievePosts"; // Note: This file contains necessary module imports
+  import Component from "vue-class-component";
+  import BigHero from "@theme/components/BigHero.vue";
+  import Articles from "@theme/components/Blog/Overview/Articles.vue";
 
-  export default {
-    name: "BlogOverview",
-    components: { BigHero, Articles },
-    mixins: [retrievePosts],
-    computed: {
-      heroImages(){
-        return this.$page.frontmatter.heroImages ? this.$page.frontmatter.heroImages : {
-          regular: "/assets/img/site/blog/typewriter.svg",
-          dark: "/assets/img/site/blog/typewriter-dark.svg"
-        };
-      },
-      pageTitle() {
-        return this.$page.title ? this.$page.title : "Blog";
-      },
-      whatToShow(){
-        if (this.$page.frontmatter.type) {
-          return this.$page.frontmatter.type;
-        }
-        else {
-          return "blogpost";
-        }
-      },
-      assets() {
-        return this.$site.themeConfig.defaultAssetPaths;
-      },
-      posts() {
-        return this.retrievePosts(this.whatToShow);
-      },
-    },
-  };
+  @Component({ name: "BlogOverview", components: { BigHero, Articles } })
+  export default class BlogOverview extends retrievePosts {
+    get heroImages() {
+      return this.$page.frontmatter.heroImages
+        ? this.$page.frontmatter.heroImages
+        : {
+            regular: "/assets/img/site/blog/typewriter.svg",
+            dark: "/assets/img/site/blog/typewriter-dark.svg",
+          };
+    }
+    get pageTitle() {
+      return this.$page.title ? this.$page.title : "Blog";
+    }
+    get whatToShow() {
+      if (this.$page.frontmatter.type) {
+        return this.$page.frontmatter.type;
+      } else {
+        return "blogpost";
+      }
+    }
+    get assets() {
+      return this.$site.themeConfig.defaultAssetPaths;
+    }
+    get posts() {
+      return this.retrievePosts(this.whatToShow);
+    }
+  }
 </script>
 
 <style lang="stylus">
+  @require '~@theme/styles/global/variables';
+
   .blog-overview-container
-    margin-top 3.6rem /* TODO: Make this dynamic, using the height of the navbar. */
+    margin-top $navigation-bar-height /* TODO: Make this dynamic, using the height of the navbar. */
     margin-bottom 5em
 
   @media screen and (prefers-color-scheme dark)
