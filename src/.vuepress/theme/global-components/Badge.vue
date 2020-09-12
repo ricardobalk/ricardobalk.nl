14b4351f6409aaad505be77d24e083d63c6af48d
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2020-09-12 21:01:19 +0200

    Added documentation to the Badge component

--- src/.vuepress/theme/global-components/Badge.vue
+++ src/.vuepress/theme/global-components/Badge.vue
@@ -1,3 +1,36 @@
+<!--
+  Component: Badge
+
+  Description:
+    Displays a badge, allows various background colours and
+    text is either provided by a slot or prop value.
+
+  Props:
+    *type['tip','green','warning','warn','yellow','error','red']
+      The type and/or background colour of the badge.
+    *text[String]
+      The text to be displayed inside the badge. The preferred way
+      is to use the slot for this.
+    *vertical['baseline','length','sub','super','top',
+      'text-top','middle','bottom','text-bottom',
+      'initial','inherit']
+            Sets the vertical alignment.
+
+  Examples:
+    <Badge>Hello World</Badge>
+      Shows a green badge with 'Hello World'.
+
+    <Badge text="Hello World"/>
+      Shows the same green badge with 'Hello World'.
+
+    <Badge type="warning">Missing configuration</Badge>
+      Shows a yellow badge with 'Missing configuration'.
+
+    <Badge type="error" vertical="top">Wrong permissions</Badge>
+      Shows a red badge with 'Wrong permissions',
+      vertically aligned on top of the current paragraph line.
+-->
+
 <template>
   <span class="badge" :class="type" :style="{ 'vertical-align': vertical }"
     ><slot>{{ text }}</slot>
