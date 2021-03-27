97330a5a2081a4d30d54835ee9de8c8b3ffd4941
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2021-03-27 19:05:12 +0100

    Changed breadcrumbs component

--- src/.vuepress/theme/components/Breadcrumbs/Breadcrumbs.ts
+++ src/.vuepress/theme/components/Breadcrumbs/Breadcrumbs.ts
@@ -1,19 +1,24 @@
-import { defineComponent, computed } from 'vue'
+import { defineComponent, computed, reactive } from 'vue'
 import type { ComputedRef } from 'vue'
 import { useRouter } from "vue-router"
-import { usePagesData } from "vuepress"
+import { resolvePageData } from "@vuepress/client"
+import { useRouteLocale } from "@vuepress/client";
+import { useThemeLocaleData } from "../../composables";
+
 import type { Breadcrumb } from './types'
 
-// TODO: Refactor this code - it is not functional
 export const Breadcrumbs = defineComponent({
    setup(){
+    const routeLocale = useRouteLocale();
+    const themeLocale = useThemeLocaleData();
+    const localePath = themeLocale.value.home ?? routeLocale.value;
+
         /**
          * Gets the path of the current page being viewed.
          * @returns a string with the current path, relative to the domain name, without trailing slash.
         */
         const currentPath = computed(() => {
-            const path : string = useRouter().currentRoute.value.path; // Closure to make code below more readable ;-)
-            console.log(`Current path: ${path}`);
+            const path : string = useRouter().currentRoute.value.path;            
             return (path.endsWith('/')) ? path.slice(0, -1) : path;
         })
 
@@ -24,30 +29,23 @@ export const Breadcrumbs = defineComponent({
        const breadcrumbs : ComputedRef<Breadcrumb[]> = computed(() => {
             /** @var currentPathArray: Current path put into an array (e.g. "/blog/article" is ['blog', 'article']) */
             const currentPathArray : Array<string> = currentPath.value.split("/");
+
+            // TODO: Remove the first breadcrumb if we're on a localised page
                 
             /** @var breadcrumbs: Breadcrumbs to be returned */
-            const breadcrumbs : Array<Breadcrumb> = [];
+            const breadcrumbs : Array<Breadcrumb> = reactive([]);
 
             /** @var link: Variable that helps the forEach loop with building up the link to the breadcrumb again */
             let link : string = "";
 
             currentPathArray.forEach(item => {
                 // Build up link from previous iteration and current iteration
-                link += item;
+                link += `${item}/`;
 
-                /** @var page: Linked page (and info) that has been found by VuePress from the given link */
-                // const page = this.$site.pages.find(el => el.path === link || el.path === link + "/");
-                
-                // Adds trailing slash, needed for next iteration
-                link += "/";
-
-                // If page exists, add it to the breadcrumbs collection
-                // page != null && breadcrumbs.push({ title: page.title, link: page.path });
+                // If page exists (can be resolved), add it to the breadcrumbs collection
+                resolvePageData(link).then( (page) => (page != null && breadcrumbs.push({ title: page.title, link: page.path })) )
             });
-
-            // Remove the first breadcrumb if we're on a localised page.
-            // breadcrumbs[0].link.indexOf(this.$localeConfig.path || "" ) === -1 && breadcrumbs.splice(0, 1);
-
+            
             return breadcrumbs;
        })
 
