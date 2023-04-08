097594685796248a2d82e133e9c73295badaf41c
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2023-04-08 22:17:06 +0200

    Added Nuxt 3 with TailwindCSS (...)
    https://github.com/ricardobalk/nuxt3-tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLayout name="hello">
+    <template #title>{{ productName }}</template>
+    <template #message>{{ message }}</template>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const productName: string = "Nuxt 3 Starter Kit";
+const features: string[] = ["Vite", "Vue 3", "TypeScript", "PostCSS", "TailwindCSS"];
+const message = computed(
+  () => `This is a ${productName} with ${features.slice(0, -1).join(", ")} and ${features.slice(-1)}!`
+);
+
+definePageMeta({
+  layout: false,
+});
+</script>
